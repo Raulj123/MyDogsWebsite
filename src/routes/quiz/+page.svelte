f21ee8ba395afe53a85c32f6277c096a65e9a42a
raulj <112786129+Raulj123@users.noreply.github.com>
2023-10-18 23:20:16 -0700

    fix: quiz UI mobile

--- src/routes/quiz/+page.svelte
+++ src/routes/quiz/+page.svelte
@@ -29,99 +29,80 @@
 </script>
 
 <div class="hero min-h-screen bg-base-200">
-  <div class="hero-content text-center flex flex-col">
-    {#if !isLast}
-      {#each questions as q}
-        {#if q === currentQues}
-          <h1 class="text-5xl primary-color font-bold">{q.question}</h1>
-          <img src={q.image} alt="doby" class="mask mask-squircle" />
-          <div class="flex flex-row">
+    <div class="hero-content text-center flex flex-col">
+  {#if !isLast}
+
+   
+    
+    {#each questions as q}
+      {#if q === currentQues}
+      <h1 class="text-5xl primary-color font-bold">{q.question}</h1>
+        <img src={q.image} alt="doby" class="mask mask-squircle">
+        <div class="flex flex-row">
             <div class="mr-4">
-              <h4 class="text-2xl">
-                <span class="secondary-color font-bold">Current Score:</span>
-                <span class="primary-color">{score}</span>
-              </h4>
+                <h4 class="text-2xl"><span class="secondary-color font-bold">Current Score:</span> <span class="primary-color">{score}</span></h4>
             </div>
             <div class="ml-4">
-              <h4 class="text-2xl">
-                <span class="primary-color">{i + 1} </span><span
-                  class="secondary-color"
-                >
-                  out of</span
-                > <span class="primary-color">{questions.length}</span>
-                <span class="secondary-color">questions left!</span>
-              </h4>
+                <h4 class="text-2xl"> <span class="primary-color">{i + 1} </span><span class="secondary-color"> out of</span> <span class="primary-color">{questions.length}</span> <span class="secondary-color">questions left!</span></h4>
             </div>
-          </div>
-          <div class="flex flex-col">
-            {#each q.anwsers as a, i}
-              <button
-                class="btn btn-outline btn-primary sm:btn-sm md:btn-md lg:btn-lg mt-3"
-                class:wrong={isWrong && clickedAns === i}
-                class:correct={(clickedAns === i && isCorrect) ||
-                  (disabled && a.isCorrect)}
-                {disabled}
-                on:click={() => {
-                  disabled = true;
-                  clickedAns = i;
-                  a.isCorrect ? (isCorrect = true) : (isWrong = true);
-                  a.isCorrect ? score++ : "";
-                  currentQues === questions[questions.length - 1]
-                    ? (isLast = true)
-                    : (isLast = false);
-                }}>{a.a}</button
-              >
-            {/each}
-          </div>
-        {/if}
-      {/each}
-
-      <div class="flex flex-row">
-        {#if i > 0}
-          <div class="me-2">
-            <button
-              class="btn btn-secondary sm:btn-sm md:btn-md lg:btn-lg"
-              on:click={prevQ}>Back</button
-            >
-          </div>
-        {/if}
-        <div class="me-2">
-          <button
-            class="btn btn-secondary sm:btn-sm md:btn-md lg:btn-lg"
-            on:click={nextQ}
-            disabled={i === questions.length - 1}>Next</button
-          >
         </div>
-      </div>
-    {:else}
-      score {score}/{questions.length}
-      <div class="card">
-        {#if score >= 4}
-          <img
-            alt="drake meme"
-            src="https://i.cbc.ca/1.3279853.1532811524!/fileImage/httpImage/drake.jpg"
-          />
-        {:else}
-          <img
-            alt="drake meme"
-            src="https://media1.metrotimes.com/metrotimes/imager/u/original/17685622/11-drake-hotline-bling.w710.h473.jpg"
-          />
-        {/if}
-      </div>
-      <button
-        class="btn btn-secondary sm:btn-sm md:btn-md lg:btn-lg"
-        on:click={() => {
-          i = 0;
-          disabled = false;
-          clickedAns = 0;
-          isCorrect = false;
-          isWrong = false;
-          isLast = false;
-          currentQues = questions[i];
-          score = 0;
-        }}>try again</button
-      >
+        <div class="flex flex-col">
+            {#each q.anwsers as a, i}
+          <button
+          class="btn btn-outline btn-primary sm:btn-sm md:btn-md lg:btn-lg mt-3"
+            class:wrong={isWrong && clickedAns === i}
+            class:correct={(clickedAns === i && isCorrect) ||
+              (disabled && a.isCorrect)}
+            {disabled}
+            on:click={() => {
+              disabled = true;
+              clickedAns = i;
+              a.isCorrect ? (isCorrect = true) : (isWrong = true);
+              a.isCorrect ? score++ : "";
+              currentQues === questions[questions.length - 1]
+                ? (isLast = true)
+                : (isLast = false);
+            }}>{a.a}</button
+          >
+        {/each}
+        </div>
+        
+      {/if}
+    {/each}
+
+    <div class="flex flex-row">
+    {#if i > 0}
+    <div class="me-2">
+      <button class="btn btn-secondary sm:btn-sm md:btn-md lg:btn-lg"on:click={prevQ}>Back</button>
+    </div>
     {/if}
+    <div class="me-2">
+    <button class="btn btn-secondary sm:btn-sm md:btn-md lg:btn-lg" on:click={nextQ} disabled={i === questions.length - 1}>Next</button>
+    </div>
+    </div>
+  {:else}
+    score {score}/{questions.length}
+    <div class="card">
+    {#if score >= 4}
+        <img alt="drake meme" src="https://i.cbc.ca/1.3279853.1532811524!/fileImage/httpImage/drake.jpg">
+    {:else}
+        <img alt="drake meme" src="https://media1.metrotimes.com/metrotimes/imager/u/original/17685622/11-drake-hotline-bling.w710.h473.jpg">
+    {/if}
+    </div>
+    <button
+     class="btn btn-secondary sm:btn-sm md:btn-md lg:btn-lg"
+      on:click={() => {
+        i = 0;
+        disabled = false;
+        clickedAns = 0;
+        isCorrect = false;
+        isWrong = false;
+        isLast = false;
+        currentQues = questions[i];
+        score = 0;
+      }}>try again</button
+    >
+  {/if}
   </div>
 </div>
 
@@ -133,8 +114,8 @@
     background-color: red;
   }
   img {
-    max-width: 450px !important;
-    max-height: 450px !important;
+    max-width: 450px;
+    max-height: 450px;
   }
   @media (max-width: 767px) {
     h1 {
